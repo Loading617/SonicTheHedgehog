3ba06439d289209750120df98dcb887cd6c4d70c
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-15 23:04:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
        	<meta charset="UTF-8">
+        <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
         <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
         <link rel="shortcut icon" href="favicon." type="image/x-icon">
         <title>Sonic The Hedgehog</title>
     </head>
@@ -9,7 +13,7 @@
     <div id="container">
     <canvas class="emuscripten" id="canvas"></canvas>
     </div>
-    <h1>Sonic The Hedgehog</h1>
+    <?php echo "<h1>Sonic The Hedgehog</h1>"; ?>
     <script type="text/javascript">
         </script>
         <script async type="text/javascript" src="sth.us.f3dex2e.js"></script>
