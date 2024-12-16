dc1789bd06adfc9558cc65e4fe079464f1273ea2
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-15 21:06:07 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+       	<meta charset="UTF-8">
+        <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="favicon." type="image/x-icon">
+        <title>Sonic The Hedgehog</title>
+    </head>
+    <body>
+    <div id="container">
+    <canvas class="emuscripten" id="canvas"></canvas>
+    </div>
+    <h1>Sonic The Hedgehog</h1>
+    <script type="text/javascript">
+        </script>
+        <script async type="text/javascript" src="sth.us.f3dex2e.js"></script>
+        <script>
+        </script>
+    </body>
+</html>		
